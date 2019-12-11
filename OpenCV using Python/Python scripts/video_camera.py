27e40b251101ed9d433a2cbd45da025ab28e2791
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-12-11 20:34:01 +0530

    Updated video_camera.py

--- OpenCV using Python/Python scripts/video_camera.py
+++ OpenCV using Python/Python scripts/video_camera.py
@@ -5,9 +5,15 @@ cap = cv2.VideoCapture(0)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+def change_contrast(image):
+    hsv_image = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
+    ch_image = cv2.equalizeHist(hsv_image[:,:,2])
+    return ch_image
+
 while True:
     ret,frame = cap.read()
 
+    frame = change_contrast(frame)
     cv2.imshow('frame',frame)
 
     if cv2.waitKey(1) & 0xFF == 27:
